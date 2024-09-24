1c5052e55f614eb7a39508f31eb981f6a6807496
HuuLam2k4 <130115010+HuuLam2k4@users.noreply.github.com>
2024-09-24 23:20:11 +0700

    hello world

--- duAn/baiTest.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-  let test = 'Hello Worlk';
-  let hello = 'chào tất cả mọi người '
-  Logger.log(test);
-  Logger.log(hello);
-}
---
+++ duAn/test.js
@@ -0,0 +1,4 @@
+function myFunction() {
+  let hello = 'Hello, world!';
+  Logger.log(hello);
+}
